be0742586e805546077bd42b1345e4afbc2aafad
Ian Coleman <ian.coleman@tacticaledge.us>
2019-01-14 11:32:50 -0500

    Added code to allow helm projectile mode to swap between tests and lib

--- custom.el
+++ custom.el
@@ -5,7 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (intero hlint-refactor haskell-snippets flycheck helm-projectile zenburn-theme org-tree-slide org-bullets fancy-narrow expand-region demo-it yasnippet which-key helm company yaml-mode use-package tagedit sublime-themes smex rainbow-delimiters projectile paredit markdown-preview-eww markdown-mode+ magit-filenotify magit-annex kotlin-mode ido-completing-read+ hindent haskell-emacs ghub+ ghc exec-path-from-shell diminish clojure-mode-extra-font-locking cider-decompile))))
+    (helm-projectile projectile intero hlint-refactor haskell-snippets flycheck zenburn-theme org-tree-slide org-bullets fancy-narrow expand-region demo-it yasnippet which-key helm company yaml-mode use-package tagedit sublime-themes smex rainbow-delimiters paredit markdown-preview-eww markdown-mode+ magit-filenotify magit-annex kotlin-mode ido-completing-read+ hindent haskell-emacs ghub+ ghc exec-path-from-shell diminish clojure-mode-extra-font-locking cider-decompile)))
+ '(projectile-haskell-cabal-compile-cmd (concat haskell-process-path-stack " build"))
+ '(projectile-haskell-cabal-test-cmd (concat haskell-process-path-stack " test"))
+ '(projectile-test-files-suffices (quote ("_test" "_spec" "Spec" "Test" "-test" "-spec")))
+ '(projectile-test-suffix-function (function es/projectile-test-suffix)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -347,6 +347,22 @@
 
     (flycheck-add-next-checker 'intero '(warning . haskell-hlint))))
 
+(defun es/projectile-test-suffix (project-type)
+  "Return test files of Spec for haskell-cabal projects
+Use -spec for all other project types"
+  (if (eq project-type 'haskell-stack)
+      "Spec"
+    "-spec"))
+
+(custom-set-variables
+ '(projectile-test-files-suffices
+   '("_test" "_spec" "Spec" "Test" "-test" "-spec"))
+ '(projectile-test-suffix-function #'es/projectile-test-suffix)
+ '(projectile-haskell-cabal-test-cmd
+   (concat haskell-process-path-stack " test"))
+ '(projectile-haskell-cabal-compile-cmd
+   (concat haskell-process-path-stack " build")))
+
 ;;; Enable Disabled Features
 
 (put 'dired-find-alternate-file 'disabled nil)
