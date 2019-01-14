a6d044cd3be9e5443d96cbd69b77cf9bcb366c46
Ian Coleman <ian.coleman@tacticaledge.us>
2019-01-14 10:33:51 -0500

    Added blank custom.el as required by init.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (intero hlint-refactor haskell-snippets flycheck helm-projectile zenburn-theme org-tree-slide org-bullets fancy-narrow expand-region demo-it yasnippet which-key helm company yaml-mode use-package tagedit sublime-themes smex rainbow-delimiters projectile paredit markdown-preview-eww markdown-mode+ magit-filenotify magit-annex kotlin-mode ido-completing-read+ hindent haskell-emacs ghub+ ghc exec-path-from-shell diminish clojure-mode-extra-font-locking cider-decompile))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
